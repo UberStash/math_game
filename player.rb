e285f25ab8b1e707586b9b846d5bcfad3e106a36
UberStash <expiredserver@gmail.com>
2020-09-15 13:01:23 +0000

    Completed game set up for file directory and have started to build game funcinality, also set up player class.

---
+++ player.rb
@@ -0,0 +1,11 @@
+class PlayerTwo
+  attr_accessor :lives, :turn
+  attr_reader :name
+
+  def initialize(year, model, color)
+    @name = name
+    @current_lives = 3
+    @turn = false
+  end
+
+end
